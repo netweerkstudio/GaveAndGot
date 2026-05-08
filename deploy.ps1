c2013467c90a08bae36dd77291f49c5b7d4ae53d
Reda Grain <grain.reda.solicode@gmail.com>
2026-05-08 20:37:53 +0100

    added deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,8 @@
-param (
-    [string]$Message = "Update site"
-)
+$Message = Read-Host "Enter commit message (leave blank for 'Update site')"
+
+if ([string]::IsNullOrWhiteSpace($Message)) {
+    $Message = "Update site"
+}
 
 Write-Host "Staging all changes..." -ForegroundColor Cyan
 git add .
